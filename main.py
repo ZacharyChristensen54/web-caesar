52cba42ed7e74169c6003880bb8595aa4d183857
ZacharyChristensen <zacharychristensen54@gmail.com>
2017-02-05 12:07:10 -0600

    In main: split message into message and encrypted message. added textarea tags in response.write arg

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ import caesar
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = caesar.encrypt("Hello world!", 13)
-        self.response.write(message)
+        message = "hello world!"
+        encrypted_message = caesar.encrypt(message, 13)
+        self.response.write("<textarea>" + encrypted_message + "</textarea>")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)

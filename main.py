578d772dfeeb82b17fc504c5cfd8340899c5a7b5
ZacharyChristensen <zacharychristensen54@gmail.com>
2017-02-05 20:07:03 -0600

    finised assignment. made global build function to recreate page after operating on submitted input and implemented it

--- main.py
+++ main.py
@@ -16,16 +16,35 @@
 #
 import webapp2
 import caesar
+import cgi
+
+def build_page(textarea_content):
+    header = "<h2>Web Caesar</h2>"
+    rot_label = "<label>Rotate by:</label>"
+    rotation_input = "<input type='number' name='rotation'/>"
+    message_label = "<label>Type a message:</label>"
+    textarea = "<textarea name='message'>" + textarea_content + "</textarea>"
+    submit = "<input type='submit'/>"
+    form = ("<form method='post'>" + 
+            rot_label + rotation_input + "<br/>" +
+            message_label + textarea + "<br/>" + 
+            submit + 
+                
+            "</form>")
+    return header + form
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = "hello world!"
-        encrypted_message = caesar.encrypt(message, 13)
+        content = build_page("")
+        self.response.write(content)
 
-        textarea = "<textarea>" + encrypted_message + "</textarea>"
-        submit = "<input type='submit'/>"
-        form = "<form>" + textarea + "<br/>" + submit + "</form>"
-        self.response.write(form)
+    def post(self):
+        rotation = int(self.request.get("rotation"))
+        message = self.request.get("message")
+        encrypted_message = caesar.encrypt(message, rotation)
+        rscaped_message = cgi.escape(encrypted_message)
+        content = build_page(encrypted_message)
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
